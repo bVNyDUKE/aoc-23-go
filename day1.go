27a7b1da9d8aa442df93f5bdffca03dee59fd92b
Darko Ivkov <bVNyDUKE@protonmail.com>
2023-12-02 00:41:29 +0100

    avoid string conversion

--- day1.go
+++ day1.go
@@ -6,48 +6,43 @@ import (
 	"os"
 	"regexp"
 	"strconv"
-	"strings"
 )
 
 type LineNums struct {
-	first  string
-	second string
+	first  int
+	second int
 }
 
 func (l *LineNums) toInt() int {
-	n, err := strconv.Atoi(strings.Join([]string{l.first, l.second}, ""))
-	if err != nil {
-		fmt.Printf("Error parsing strings %v \n", err)
-	}
-	return n
+	return (l.first * 10) + l.second
 }
 
-var digits = map[string]string{
-	"one":   "1",
-	"two":   "2",
-	"three": "3",
-	"four":  "4",
-	"five":  "5",
-	"six":   "6",
-	"seven": "7",
-	"eight": "8",
-	"nine":  "9",
+var digits = map[string]int{
+	"one":   1,
+	"two":   2,
+	"three": 3,
+	"four":  4,
+	"five":  5,
+	"six":   6,
+	"seven": 7,
+	"eight": 8,
+	"nine":  9,
 }
 
 var re = regexp.MustCompile(`(one|two|three|four|five|six|seven|eight|nine)`)
 
-func checkStrings(first byte, second string) string {
+func checkStrings(first byte, second string) int {
 	stringChar := string(first)
-	_, err := strconv.Atoi(stringChar)
+	val, err := strconv.Atoi(stringChar)
 	if err == nil {
-		return stringChar
+		return val
 	}
 
 	matched := re.FindString(second)
 	if matched != "" {
 		return digits[matched]
 	}
-	return ""
+	return 0
 }
 
 func main() {
@@ -62,21 +57,21 @@ func main() {
 		lineNums := LineNums{}
 		f, s := 0, len(line)-1
 		for {
-			if lineNums.first != "" && lineNums.second != "" {
+			if lineNums.first != 0 && lineNums.second != 0 {
 				break
 			}
 
-			if lineNums.first == "" {
+			if lineNums.first == 0 {
 				res := checkStrings(line[f], line[:f+1])
-				if res != "" {
+				if res != 0 {
 					lineNums.first = res
 				}
 
 				f++
 			}
-			if lineNums.second == "" {
+			if lineNums.second == 0 {
 				res := checkStrings(line[s], line[s:])
-				if res != "" {
+				if res != 0 {
 					lineNums.second = res
 				}
 				s--
